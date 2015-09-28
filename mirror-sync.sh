e7a252293dd5306f47bddbe6ef05fc641e80c4d5
Alejandro Suarez <alejandroscf@gmail.com>
2015-09-28 15:51:02 +0200

    Added GPL header and "set" to exit on errors

--- mirror-sync.sh
+++ mirror-sync.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
-#################################################################
-##########       UPDATE SCRIPT
-#################################################################
+
+##    Alejandro Suarez Cebrian (Responsable de la Oficina de Software Libre de la Universidad de Zaragoza)
+##    Copyright (C) 2015  Alejandro Suarez Cebrian
+##
+##    This program is free software: you can redistribute it and/or modify
+##    it under the terms of the GNU General Public License as published by
+##    the Free Software Foundation, either version 3 of the License, or
+##    any later version.
+##
+##    This program is distributed in the hope that it will be useful,
+##    but WITHOUT ANY WARRANTY; without even the implied warranty of
+##    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##    GNU General Public License for more details.
+##
+##    You should have received a copy of the GNU General Public License
+##    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+# Exit on errors and unset variables
+set -e
+set -u
 
 export PATH='/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin'
 
